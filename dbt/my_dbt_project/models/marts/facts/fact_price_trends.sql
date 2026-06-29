fa27c647a38a744d1d028a37fad432bf9539d304
Tamara Srbinoska <tamarasrbinoska@Tamaras-MacBook-Air.local>
2026-06-29 13:24:16 +0200

    Implement dbt tests and finalize DW models

---
+++ dbt/my_dbt_project/models/marts/facts/fact_price_trends.sql
@@ -0,0 +1,13 @@
+{{ config(materialized='table') }}
+
+select
+    da.asset_key,
+    dd.date_key,
+    p.moving_avg_7d,
+    p.moving_avg_14d,
+    p.moving_avg_30d
+from {{ ref('price_with_averages') }} p
+join {{ ref('dim_asset') }} da
+    on p.coin_id = da.symbol
+join {{ ref('dim_date') }} dd
+    on p.date = dd.full_date
